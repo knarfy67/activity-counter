fc8ac6f9cbcacf499e11f2e35b3e9616eb2e94b2
knarfy67 <occ.arbiso.frank@gmail.com>
2023-10-14 10:05:41 +0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,24 @@
+"use client"
+import { useState } from 'react';
+
+export default function Counter(){
+  const[count,setCOunt]=useState(0);
+
+  const increment =()=>{
+    setCOunt(count +1);
+
+  }
+  const decrement =()=>{
+    setCOunt(count -1)
+  }
+  return(
+    <div>
+      <center>
+      <h1>Counter</h1>
+      <p>Current count:{count}</p>
+      <button onClick={increment}>INCREMENT</button>
+      <button onClick={decrement}>DECREMENT</button>
+      </center>
+    </div>
+  )
+}
